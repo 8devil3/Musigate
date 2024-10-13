be0b79b2b57362b495266d7493aad9a9f312755d
OrangeWeb <8dev.il3@gmail.com>
2024-10-13 17:52:16 +0200

    corretto checkbox

--- resources/js/Components/Form/Checkbox.vue
+++ resources/js/Components/Form/Checkbox.vue
@@ -8,7 +8,7 @@
             :required="props.required"
             class="size-4 shadow-sm md:mt-[1px] rounded-[4px] border-2 border-orange-500 bg-slate-900 text-orange-500 cursor-pointer"
         >
-        <label v-if="$slot"  :for="props.id" class="text-xs leading-none text-gray-300 cursor-pointer md:text-sm mt-[2px] md:mt-0">
+        <label :for="props.id" class="text-xs leading-none text-gray-300 cursor-pointer md:text-sm mt-[2px] md:mt-0">
             <slot />
         </label>
     </div>
