1a1b51b7aa9924b4116a0a859bbb8308870808b1
OrangeWeb <8dev.il3@gmail.com>
2024-10-13 15:34:59 +0200

    disattivati seeder booking

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,9 +40,9 @@ class DatabaseSeeder extends Seeder
             VideoSeeder::class,
             RoomPhotoSeeder::class,
 
-            BookingSettingSeeder::class,
-            CancelPolicySettingSeeder::class,
-            BookingSeeder::class,
+            // BookingSettingSeeder::class,
+            // CancelPolicySettingSeeder::class,
+            // BookingSeeder::class,
         ]);
     }
 }
