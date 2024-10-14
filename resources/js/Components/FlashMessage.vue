88e1cd3fefb9d2a23e95b4ca39dd73fc17bfe100
OrangeWeb <8dev.il3@gmail.com>
2024-10-14 20:26:36 +0200

    corretto css flashmessage

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -2,16 +2,18 @@
     <teleport to="#flash-message">
         <Transition leave-active-class="transition duration-1000 ease-in" leave-from-class="opacity-100" leave-to-class="opacity-0">
             <div v-show="success" class="fixed z-50 -translate-x-1/2 top-8 lg:top-4 left-1/2">
-                <div class="flex items-center gap-2 px-4 py-2 text-sm font-medium leading-none text-white border-2 rounded-full shadow-lg bg-emerald-900 border-emerald-500">
+                <div class="flex items-center gap-2 px-3 py-2 text-xs font-medium leading-none text-white border-2 rounded-full shadow-lg md:gap-3 md:text-sm whitespace-nowrap bg-emerald-900 border-emerald-500">
                     <i class="fa-solid fa-check" />
                     {{ usePage().props.flash.success }}
                 </div>
             </div>
         </Transition>
 
-        <div v-show="usePage().props.flash.error" class="flex items-center gap-2 p-2 text-sm font-medium leading-none text-white bg-red-600 border-2 border-red-900 rounded-full shadow-lg">
-            <i class="fa-solid fa-circle-exclamation" />
-            Salvataggio non riuscito!
+        <div v-show="usePage().props.flash.error" class="fixed z-50 -translate-x-1/2 top-8 lg:top-4 left-1/2">
+            <div class="flex items-center gap-2 px-3 py-2 text-xs font-medium leading-none text-white bg-red-900 border-2 border-red-500 rounded-full shadow-lg md:gap-3 md:text-sm whitespace-nowrap">
+                <i class="fa-solid fa-circle-exclamation" />
+                Salvataggio non riuscito!
+            </div>
         </div>
     </teleport>
 </template>
@@ -27,7 +29,7 @@ const dissolveFlashSuccess = ()=>{
 
     let timeoutId = setTimeout(() => {
         success.value = false;
-    }, 2000);
+    }, 2000000);
 };
 
 onBeforeUpdate(()=>{
