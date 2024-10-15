d194119ecde5af6c846215ae64f1e1036ed48787
OrangeWeb <8dev.il3@gmail.com>
2024-10-15 21:36:02 +0200

    tradotti altri messaggi di validazione e autenticazione

--- lang/it/auth.php
+++ lang/it/auth.php
@@ -13,8 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'failed' => 'Credenziali non presenti.',
+    'password' => 'La password non è corretta.',
+    'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
 ];
--- lang/it/passwords.php
+++ lang/it/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'La tua password è stata reimpostata.',
+    'sent' => 'Ti abbiamo inviato via email il link per reimpostare la password.',
+    'throttled' => 'Attendi prima di riprovare.',
+    'token' => 'Questo token per la reimpostazione della password non è valido.',
+    'user' => 'Impossibile trovare un utente con quell’indirizzo email.',
 
 ];
