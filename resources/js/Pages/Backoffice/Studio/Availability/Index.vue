2ae93426ef739f88d5a23fd506538330185126db
OrangeWeb <8dev.il3@gmail.com>
2024-10-19 01:36:11 +0200

    rimosso codice inutile

--- resources/js/Pages/Backoffice/Studio/Availability/Index.vue
+++ resources/js/Pages/Backoffice/Studio/Availability/Index.vue
@@ -5,7 +5,6 @@
         icon="fa-solid fa-clock"
     >
         <template #content>
-            <!-- copia disponibilità -->
             <FormElement>
                 <template #description>
                     Click o tap su un giorno della settimana per impostare o modificare gli orari e le fasce orarie.
@@ -31,129 +30,19 @@
                     </div>
                 </template>
             </FormElement>
-            <!-- / -->
         </template>
     </ContentLayout>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useForm, router, Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/vue3';
 import ContentLayout from '@/Layouts/Backoffice/ContentLayout.vue';
 import FormElement from '@/Components/Backoffice/FormElement.vue';
-import Toggle from '@/Components/Form/Toggle.vue';
-import Select from '@/Components/Form/Select.vue';
-import SaveButton from '@/Components/Form/SaveButton.vue';
-import Empty from '@/Components/Backoffice/Empty.vue';
-import InfoBlock from '@/Components/InfoBlock.vue';
-import Button from '@/Components/Form/Button.vue';
-import ActionButton from '@/Components/Form/ActionButton.vue';
-import Input from '@/Components/Form/Input.vue';
 
 const props = defineProps({
     availability: Object,
-    timebands: Object,
-    current_weekday: Number,
-    hours: Object,
     weekdays: Object,
 });
-
-const hasValidationErrors = ref(false);
-const cloneFromWeekday = ref('');
-
-const form = useForm({
-    current_weekday: props.current_weekday,
-    is_open: props.availability.is_open,
-    open_start: props.availability.open_start,
-    open_end: props.availability.open_end,
-    timebands: props.timebands ?? [],
-});
-
-const submit = ()=>{
-    if(form.processing || hasValidationErrors.value) return;
-    form.put(route('studio.availability.update'), {
-        preserveState: false,
-    });
-};
-
-const selectWeekday = ()=>{
-    router.get(route('studio.availability.edit'), {current_weekday: form.current_weekday}, {
-        preserveState: false
-    });
-};
-
-const cloneAvailability = ()=>{
-    router.put(route('studio.availability.clone'), {current_weekday: form.current_weekday, clone_from_weekday: cloneFromWeekday.value}, {
-        preserveState: false
-    });
-};
-
-const addTimeband = ()=>{
-    let timeband = {
-        id: null,
-        name: null,
-        start: form.open_start,
-        end: null,
-    };
-
-    if(form.timebands.length){
-        let index = form.timebands.length;
-        timeband.start = form.timebands[index -1].end;
-        timeband.end = null;
-    }
-
-    form.timebands.push(timeband);
-
-    validations();
-};
-
-const deleteTimeband = (index)=>{
-    form.timebands.splice(index, 1);
-    validations();
-};
-
-const setTimebandsStartEnd = ()=>{
-    form.timebands.forEach((timeband, index) => {
-        if(index) timeband.start = form.timebands[index -1].end;
-        else timeband.start = form.open_start;
-    });
-};
-
-const validations = ()=>{
-    setTimebandsStartEnd();
-
-    //validazione orari delle fasce orarie
-    hasValidationErrors.value = false;
-    if(
-        form.timebands.length &&
-        (form.timebands[0].start !== form.open_start ||
-        form.timebands[form.timebands.length -1].end !== form.open_end)
-    ){
-        hasValidationErrors.value = {
-            title: 'Orario inizio/fine non coerente con apertura/chiusura',
-            message: 'Gli orari di inizio e fine della prima e ultima fascia oraria devono coincidere con quelle di apertura e chiusura dello Studio.'
-        };
-    }
-
-    //validazione numero di fasce orarie (minimo 2)
-    if(form.timebands.length && form.timebands.length < 2){
-        hasValidationErrors.value = {
-            title: 'Fascia oraria singola',
-            message: 'Devi inserire almeno due fasce orarie.'
-        };
-    }
-
-    //validazione nomi univoci
-    let timebandNames = form.timebands.map(tb => tb.name);
-    let hasDuplicates = new Set(timebandNames).size !== timebandNames.length;
-    
-    if(hasDuplicates){
-        hasValidationErrors.value = {
-            title: 'Nome fascia non univoco',
-            message: 'Le fasce orarie devono avere nomi univoci nello stesso giorno. Possono avere lo stesso nome in giorni diversi.'
-        };
-    }
-};
 
 </script>
 
