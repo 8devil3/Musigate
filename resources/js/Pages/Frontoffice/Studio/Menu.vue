b8a3fefd6993eb79030fed67b1276a43ba988be5
OrangeWeb <8dev.il3@gmail.com>
2024-10-21 02:06:15 +0200

    corretta spaziatura menu studio show

--- resources/js/Pages/Frontoffice/Studio/Menu.vue
+++ resources/js/Pages/Frontoffice/Studio/Menu.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="sticky top-0 z-40 border-slate-700 border-y bg-slate-950/40 backdrop-blur-sm">
-        <ul class="flex items-center w-full max-w-6xl gap-8 px-4 mx-auto overflow-x-auto md:px-6">
+        <ul class="flex items-center w-full max-w-6xl gap-6 px-4 mx-auto overflow-x-auto md:px-6">
             <template v-for="link in props.links">
                 <li v-if="link.enabled">
                     <a :href="link.id" class="block py-3 text-xs tracking-wide uppercase transition-colors border-b-2 border-transparent font-lemon lg:text-sm lg:py-4 whitespace-nowrap hover:border-orange-500 active:border-orange-500">
