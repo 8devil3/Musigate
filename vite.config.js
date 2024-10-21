508e475b835802b3d263bc6085c101dbac699ee9
8devil3 <8dev.il3@gmail.com>
2024-10-21 23:33:45 +0200

    aggiunto Vite PWA e aggiornati pacchetti

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,77 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            // includeAssets: ['/offline.html'],
+            disable: false,
+            devOptions: {
+                enabled: false,
+                type: 'module',
+            },
+            outDir: 'public/build',
+            base: 'public',
+            buildBase: '/build/',
+            scope: '/build/',
+            workbox: {
+                navigateFallback: null,
+                cleanupOutdatedCaches: true,
+                directoryIndex: null, // this prevents fallback to index.html,
+                additionalManifestEntries: [
+                    {
+                        url : '/offline.html',
+                        revision: `${Date.now()}`
+                    },
+                ]
+            },
+            manifest: {
+                name: 'Musigate',
+                short_name: 'Musigate',
+                description: 'Musigate | SuonoErgoSono.',
+                orientation: 'portrait-primary',
+                theme_color: '#020617',
+                id: '/',
+                start_url: '/',
+                scope: '/',
+                display: 'standalone',
+                background_color: '#020617',
+                lang: 'it',
+                icons: [
+                    {
+                        src: '/img/pwa/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/img/pwa/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/img/pwa/mask-icon.png',
+                        sizes: '196x196',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    },
+                ],
+                // screenshots: [
+                //     {
+                //         src: '/img/pwa/screenshot_desktop.png',
+                //         sizes: '1280x714',
+                //         type: 'image/png',
+                //         form_factor: 'wide',
+                //         label: 'RemoteCareers'
+                //     },
+                //     {
+                //         src: '/img/pwa/screenshot_mobile.png',
+                //         sizes: '320x570',
+                //         type: 'image/png',
+                //         form_factor: 'narrow',
+                //         label: 'RemoteCareers'
+                //     },
+                // ],
+            }
         }),
     ],
 });
